f32059b9f378b55c8545c1ec2fa47e0a655fff8d
Trippinhawk <trippinhawk@yahoo.com>
2023-03-09 11:50:55 +0100

    Result Page

--- views.js
+++ views.js
@@ -38,3 +38,100 @@ function LandingPageView(){
     return container
 }
 
+
+
+function ResultPageView(){
+
+    let container = document.createElement("div")
+    let utloging = document.createElement("button")
+    let getPdf = document.createElement("button")
+    let shareLink = document.createElement("button")
+
+    let settings = document.createElement("button")
+    settings.onclick = function(){
+        goToSettings()
+    }
+    
+    let qBox = document.createElement("div")
+    for(let i = 0; i<model.questions.length; i++){
+        let qCard = document.createElement("div")
+
+        qBox.appendChild(qCard)
+
+        let qText = document.createElement("div")
+        qText.textContent = model.questions[i].question
+        qCard.appendChild(qText)
+
+        let voteCount = 0;
+        for(let j = 0; j<model.questions[i].answers.length; j++){
+            voteCount += model.questions[i].answers[j].counter
+        }
+
+        for(let j = 0; j<model.questions[i].answers.length; j++){
+            let aText = document.createElement("div")
+            aText.textContent = model.questions[i].answers[j].title
+
+            let aCount = document.createElement("label")
+            aCount.textContent = model.questions[i].answers[j].counter
+
+            let countPercent = document.createElement("label")
+            countPercent.textContent = `${model.questions[i].answers[j].counter / voteCount * 100} %`
+
+            let percentBar = document.createElement("div")
+            percentBar.style.backgroundColor = 'green'
+            percentBar.style.width = model.questions[i].answers[j].counter / voteCount * 100
+
+            aCount.appendChild(percentBar)
+            qCard.appendChild(aText)
+            qCard.appendChild(aCount)
+            qCard.appendChild(countPercent)
+        }
+
+        if(model.questions[i].available == false){
+
+            let lockIcon = document.createElement("img")
+            lockIcon.src = "lock.png"
+            
+
+            let deleteButton = document.createElement("button")
+            deleteButton.onclick = function(){
+                deleteQuestion(i)
+            }
+
+            let dateAndTime = document.createElement("input")
+            dateAndTime.setAttribute("type","date")
+            dateAndTime.oninput = function(){
+                model.inputs.resultpage.currentQuestions[i].dateAndTime = this.value
+            }
+
+            let openPoll = document.createElement("button")
+            openPoll.onclick = function(){
+                reOpenPoll(i)
+            } 
+            
+            qCard.appendChild(lockIcon)
+            qCard.appendChild(deleteButton)
+            qCard.appendChild(dateAndTime)
+            qCard.appendChild(openPoll)
+        }            
+        
+        
+        
+
+    }
+    
+    container.appendChild(settings)
+    container.appendChild(getPdf)
+    container.appendChild(shareLink)
+    container.appendChild(utloging)
+    container.appendChild(qBox)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}

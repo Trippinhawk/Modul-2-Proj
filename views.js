5646442974ac04e434a0a3f81ac6319a73bdd431
abir314 <121821310+abir314@users.noreply.github.com>
2023-03-09 12:38:56 +0100

    settings page left side

--- views.js
+++ views.js
@@ -1,9 +1,11 @@
 
 
 const infoDiv = document.getElementById('app');
-
+updateView();
 function updateView(){
-    infoDiv.innerHTML = ""
+    infoDiv.innerHTML = "";
+
+    infoDiv.appendChild(lagetQuestions());
 
     switch(model.app.currentView){
         case "LandingPage":
@@ -14,22 +16,17 @@ function updateView(){
 }
 function LandingPageView(){
     let container = document.createElement("div")
-
     for (let i = 0; i<model.questions.length; i++){
         let box = document.createElement("div")
         box.className="QuestionContainer"
         box.textContent = model.questions[i].question
-
         let list = document.createElement("div")
-
         for(let j=0;j<model.questions[i].answers.length;j++){
             let boxChild = document.createElement("div")
             boxChild.textContent = model.questions[i].answers[j].title
-
             let inputBoks = document.createElement("input")
             inputBoks.setAttribute("type","checkbox")
             inputBoks.textContent = model.questions[i].answers[j].title
-            
             list.appendChild(inputBoks)
             box.appendChild(list)
         }

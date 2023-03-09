85cb68c50637067e1e1576cff9a3c006b848066d
Fylkirist <112450556+Fylkirist@users.noreply.github.com>
2023-03-09 09:26:38 +0100

    readability

--- views.js
+++ views.js
@@ -14,17 +14,22 @@ function updateView(){
 }
 function LandingPageView(){
     let container = document.createElement("div")
+
     for (let i = 0; i<model.questions.length; i++){
         let box = document.createElement("div")
         box.className="QuestionContainer"
         box.textContent = model.questions[i].question
+
         let list = document.createElement("div")
+
         for(let j=0;j<model.questions[i].answers.length;j++){
             let boxChild = document.createElement("div")
             boxChild.textContent = model.questions[i].answers[j].title
+
             let inputBoks = document.createElement("input")
             inputBoks.setAttribute("type","checkbox")
             inputBoks.textContent = model.questions[i].answers[j].title
+            
             list.appendChild(inputBoks)
             box.appendChild(list)
         }
